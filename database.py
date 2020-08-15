8b6f326264d5105eff99295d014f9240d1edac11
magusch <magusch@gmail.com>
2020-08-15 17:02:18 +0300

    new architect

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ import psycopg2
 
 
 #CREATE TABLE posts (title varchar(100), posttext varchar, post_id int);
-#CREATE TABLE exhibitions (day int,month int,title varchar(150), post_id int);
+#CREATE TABLE exhibitions (post_id INT UNIQUE, title varchar(150), date_before TIMESTAMP);
 
 import os
 from dotenv import load_dotenv
@@ -13,54 +13,44 @@ if os.path.exists(dotenv_path):
 
 DATABASE_URL =os.environ.get('DATABASE_URL')
 
-conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+#conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 
 url='https://t.me/DavaiSNami/'
 monthes=['января', "февраля", 'марта', 'апреля', 'мая', 'июня','июля','августа','сентября','октября','ноября','декабря']
 
-def savepost(post, message_id):
-	title=post[:post.find('\n')].strip('\u200b')
-	lineoftitle=title.split(' ')
-	month=list(set(lineoftitle) & set(monthes))
-	numbBegTitle=title.find(month[-1])
-	date=title[:numbBegTitle]
-	title=title[numbBegTitle+len(month[-1])+1:]
-	#date=date[:date.index(month[-1])+1]
-	if re.search(r'[–-]',date):
-		dates=re.split(r'[–-]', date)
-		dates=[*range(int(dates[0]), int(dates[1])+1)]
-	else:
-		dates=re.split(r'[и,]', date)
-		for o in range(len(dates)):
-			dates[o]=int(dates[o])
+TAGS_EVENTS = ["id", "title", "post_id"]
+TABLENAME_EVENTS = "events"
 
 
-	#link=url+str(message_id)
-	#data=[datesJSON, month[0], title, link]
-	
-	with conn.cursor() as cursor:
-		for dateInt in dates:
-			query = 'INSERT INTO datepost (dates,month,post_id) VALUES (%s,%s,%s)' 
-			cursor.execute(query,(dateInt,month[0], message_id))
-		query = 'INSERT INTO posts (title,post_id,posttext) VALUES (%s,%s,%s)' 
-		cursor.execute(query,(title,message_id, post))
-		conn.commit()
+def get_db_cursor():
+	return psycopg2.connect(DATABASE_URL, sslmode='require').cursor()
+
+def _get(script):
+	db_cursor = get_db_cursor()
+	db_cursor.execute(script)
+	values = db_cursor.fetchall()
+	db_cursor.close()
+	return values
+
+def _insert(script, data):
+	"""
+	    Parameters:
+	    -----------
+	    data : list of values
+	        inserting data
+	    script : str
+	        executing script
+	    """
+	db_cursor = get_db_cursor()
+
+	db_cursor.execute(script, data)
+	db_connection.commit()
+	db_connection.close()
+	db_cursor.close()
 
 
-def events(mess):
-	dateRequest, monthRequest=date_from_mess(mess)
-	Message=str(mess)+':\n'
-	with conn.cursor() as cursor:
-		query="SELECT post_id FROM datepost WHERE dates=%s AND month=%s"
-		cursor.execute(query, (dateRequest,monthRequest))
-		post_ids = cursor.fetchall()
-		for postid in post_ids:
-			title="SELECT title, post_id FROM posts WHERE post_id=%s"
-			cursor.execute(title, postid)
-			titles=cursor.fetchall()
-			Message=Message +'[%s](https://t.me/DavaiSNami/%s)\n'%(titles[0][0], titles[0][1])
-	return Message
+
 
 def date_from_mess(mess):
 	Message=str(mess)+':\n'
@@ -70,33 +60,53 @@ def date_from_mess(mess):
 		raise IOError("not correct data")
 	return dateRequest, monthRequest
 
-def save_exib(post,message_id):
-	title=post[:post.find('\n')].strip('\u200b')
-	lineoftitle=title.split(' ')
-	day=int(lineoftitle[1])
-	month=monthes.index(lineoftitle[2])+1
-	title=' '.join(lineoftitle[3:])
-	#link=url+str(message_id)
-	
-	with conn.cursor() as cursor:
-		query = 'INSERT INTO exhibitions (day,month,title, post_id) VALUES (%s,%s,%s, %s)' 
-		cursor.execute(query,(day,month, title, message_id))
-		conn.commit()
+def get_message_with_events(dt):
+	message = f"*{dt.day} {monthes[dt.month-1]}:*\n"
+
+	events = event_by_date(dt)
+
+	for event in events:
+		message += '[%s](https://t.me/DavaiSNami/%s)\n' %(event['title'], event['post_id'])
+
+	return message
 
 
-def exhib_find(day_today,month_today):
+def event_by_date(dt):
+    """
+    Required for dt (type datetime):
+    Only year, month and day.
+    """
+    script = (
+        f"SELECT {', '.join(TAGS_EVENTS)} FROM {TABLENAME_EVENTS} "
+        "WHERE post_id IS NOT NULL "
+        "AND EXTRACT(DAY FROM date_from) = %s " 
+        "AND EXTRACT(MONTH FROM date_from) = %s " %(dt.day, dt.month)
+    )
+
+    events = list()
+    for values in _get(script):
+        events.append(
+            dict(title=values[1], post_id=values[2])
+        )
+
+    return events
+
+def save_exibition(exib):
+	script = "INSERT INTO exhibitions (post_id, title, date_before) VALUES (%s, %s, cast('%s' as TIMESTAMP))"
+	data = [event['post_id'], event['title'], event['date_before']]
+	_insert(script, data)
+
+
+def find_exibitions(date_today):
 	#dateRequest, monthRequest=date_from_mess(mess)
 	#monthRequest=monthes.index(monthRequest)+1
-
-	with conn.cursor() as cursor:
-		query="SELECT title, post_id FROM exhibitions WHERE day>=%s AND month>=%s;"
-		cursor.execute(query, (day_today,month_today))
-		post = cursor.fetchall()
+	script = "SELECT title, post_id FROM exhibitions \
+	WHERE date_before>=cast('%s' as TIMESTAMP)" %(date_today)
 
 	message='*Выставки:*\n\n'
-	for p in post:
+	for exib in _get(script):
 		#link=url+str(p[1])
-		message=message+'[%s](https://t.me/DavaiSNami/%s)\n' %(p[0],p[1])
+		message=message+'[%s](https://t.me/DavaiSNami/%s)\n' %(exib[0],exib[1])
 	return message
 
 
---
+++ run.py
@@ -0,0 +1,128 @@
+import telebot
+from telebot import types
+
+import os, re
+from datetime import datetime 
+from flask import Flask, request
+from database import get_message_with_events, save_exibition, find_exibitions
+from analysis import get_day
+
+from dotenv import load_dotenv
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path)
+
+token= os.environ['token']
+
+
+URL = os.environ['URL']
+#PORT = int(os.environ.get('PORT'))
+id_admin=os.environ['id_admin']
+
+
+bot = telebot.TeleBot(token)
+
+
+server = Flask(__name__)
+
+#month_int2name=["января","февраля","марта","апреля","мая","июня","июля","августа","сентября","октября", "ноября", "декабря"]
+month_int2name=["янв","фев","мар","апр","мая","июн","июл","авг","сен","окт", "ноя", "дек"]
+
+markup = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
+date_menu=['сегодня', 'завтра', 'выходные', 'выставки']
+markup.add(types.KeyboardButton(date_menu[0].capitalize()), types.KeyboardButton(date_menu[1].capitalize()))
+markup.add(types.KeyboardButton(date_menu[2].capitalize()), types.KeyboardButton(date_menu[3].capitalize()))
+
+
+
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, "Привет! Это бот канала @DavaiSNami. Здесь можно получить краткий гид о мероприятиях на определённый день. Вся информация берётся с канала! \n Просто напишите дату в формате «31 декабря», «31.12» или «Сегодня» («Завтра», «Выходные»).", reply_markup=markup)
+
+@bot.message_handler(content_types=['text', 'photo'])
+def send_text(message):
+	#bot.send_message(message.chat.id, 'Одну секунду')
+	message_from_user = message.text.lower()
+	daynow = datetime.now()
+
+	if message_from_user==date_menu[0]:
+		date_with_events = get_day(0)
+		messg=get_message_with_events(date_with_events)
+
+	elif message_from_user==date_menu[1]:
+		date_with_events=get_day(1)
+		messg=get_message_with_events(date_with_events)
+
+	elif message_from_user==date_menu[2]:
+		date_with_events = get_day(6)
+		messg = get_message_with_events(date_with_events)
+		date_with_events = get_day(7)
+		messg=messg+'\n'+get_message_with_events(date_with_events)
+
+	elif message_from_user==date_menu[3]:
+		messg=find_exibitions(daynow)
+
+	else:
+		mq=re.split(r'[:,./ ]',message_from_user)
+		try:
+			day = int(mq[0])
+			if len(mq)<2:
+				month = daynow.month
+				if day < daynow.day: month+=1
+			elif re.search(r'[\d+]', mq[1]): #TODO: check month value
+				month = int(mq[1])
+			elif mq[1][:3] in month_int2name:
+				month = month_int2name.index(mq[1][:3])+1			
+			else:
+				month = daynow.month
+				if day < daynow.day: month+=1
+
+			date_with_events = datetime(day=day, month=month, year=daynow.year)
+			messg=get_message_with_events(date_with_events)
+		except:
+			bad_message = message_from_user+' from @%s (%s %s)' %(message.from_user.username, message.from_user.first_name, message.from_user.last_name)
+			bot.send_message(id_admin, bad_message)
+			messg='Некорректная дата'
+	bot.send_message(message.chat.id, messg, parse_mode="Markdown", disable_web_page_preview=True, reply_markup=markup)
+	
+
+
+@bot.channel_post_handler(content_types=['text', 'photo'])
+def take_post_fromChannel(message):
+	if message.content_type =='text':
+		post=message.text
+	else:
+		post=message.caption
+	if post[:2]=='До':
+		daynow = datetime.now()
+		exib=dict()
+
+		try:
+			title_line = post[:post.find('\n')].strip('\u200b')
+			title_list = title_line.split(' ')
+
+			exib['date_before'] = datetime(day = int(title_list[1]), month = monthes.index(title_list[2])+1, year=daynow.year) 
+			exib['title'] = ' '.join(title_list[3:])
+			exib['post_id'] = message.message_id
+			
+			save_exibition(exib)
+			#exbns="%s: http://davaisnami.magusch.ru/?post=%s"%(title,str(message.message_id))
+		except:
+			bot.send_message(id_admin, 'Ошибка')
+
+
+bot.polling()
+
+# @server.route("/webhook", methods=['POST'])
+# def getMessage():
+# 	bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+# 	return "!", 200
+# @server.route("/")
+# def webhook():
+# 	bot.remove_webhook()
+# 	bot.set_webhook(url=URL+"/webhook") # эurl нужно заменить на url вашего Хероку приложения
+# 	return "?", 200
+
+# server.run(host="0.0.0.0", port=os.environ.get('PORT', 80))
